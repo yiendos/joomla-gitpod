4ceaaebeffa1f05b471bef04024f3ca9cb5f7faa
yiendos <yiendos@sbmatrix.com>
2020-05-13 16:37:58 +0100

    #1: move install to bootstrap

--- .gitpod/bootstrap.sh
+++ .gitpod/bootstrap.sh
@@ -2,5 +2,13 @@
 
 set -e
 
+export PATH=/home/gitpod/.composer/vendor/bin/:$PATH
+
+joomla plugin:install joomlatools/console-joomlatools:dev-master
+
+echo "* Create a new Joomla site"
+
+joomla site:create joomla --disable-ssl --mysql-login=root:
+
 echo "* Launch preview pane"
 gp preview $(gp url 8080)
